a77473412b7e6ca17870bcb667d6f2e408e9eb04
srcampero <acy78@hotmail.com>
2026-06-13 15:58:25 -0600

    Add files via upload

--- init_db.py
+++ init_db.py
@@ -16,17 +16,16 @@ def crear_base_datos():
         )
     ''')
     
-    # Limpiar datos previos para pruebas
+# Limpiar datos previos para pruebas
     cursor.execute("DELETE FROM productos")
     
-    # Tus productos iniciales exactos
+    # Tus productos iniciales exactos con la URL corregida a medio_pollo
     productos = [
-        ("Pollo Estilo Broaster (1 Entero)", "10 piezas de delicioso pollo crujiente hecho al momento. Incluye: salsa, papas fritas, ensalada de col y tortillas calientes. ¡Listo para llevar a casa!", 270.00, "pollo_entero.jpg"),
-        ("Pollo Estilo Broaster (1/2 Pollo)", "5 piezas de delicioso pollo crujiente hecho al momento. Incluye: salsa, papas fritas, ensalada de col y tortillas calientes. ¡Listo para llevar a casa!", 150.00, "medio_pollo.jpg"),
-        ("Crunchy Burguer", "Hamburguesa con filete de pechuga de pollo empanizado súper crujiente, vegetales frescos y aderezo especial en pan suave.", 80.00, "crunchy_burguer.jpg"),
-        ("Paquete Crunchy Burguer", "Nuestra Crunchy Burguer acompañada de una buena porción de papas fritas doradas y refresco de 355 ml.", 110.00, "paquete_crunchy.jpg")
+        ("Pollo Estilo Broaster (1 Entero)", "10 piezas de delicioso pollo crujiente hecho al momento, Incluye: salsa, papas fritas, ensalada de col y tortillas calientes, ¡Listo para llevar a casa!", 270.00, "https://raw.githubusercontent.com/Alfe2801/srcamperobot/main/static/images/pollo_entero.jpg"),
+        ("Pollo Estilo Broaster (1/2 Pollo)", "5 piezas de delicioso pollo crujiente hecho al momento, Incluye: salsa, papas fritas, ensalada de col y tortillas calientes, ¡Listo para llevar a casa!", 150.00, "https://raw.githubusercontent.com/Alfe2801/srcamperobot/main/static/images/medio_pollo.jpg"),
+        ("Crunchy Burguer", "Hamburguesa con filete de pechuga de pollo empanizado súper crujiente, vegetales frescos y aderezo especial en pan suave.", 80.00, "https://raw.githubusercontent.com/Alfe2801/srcamperobot/main/static/images/crunchy_burguer.jpg"),
+        ("Paquete Crunchy Burguer", "Nuestra Crunchy Burguer acompañada de una buena porción de papas fritas doradas y refresco de 355 ml.", 110.00, "https://raw.githubusercontent.com/Alfe2801/srcamperobot/main/static/images/paquete_crunchy.jpg")
     ]
-    
     cursor.executemany('''
         INSERT INTO productos (nombre, descripcion, precio, imagen_url)
         VALUES (?, ?, ?, ?)
