efa3db85730c5f0ae58c4d5cd909240a35be4008
srcampero <acy78@hotmail.com>
2026-06-14 14:18:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,24 +31,23 @@ def index():
     productos = query_db('SELECT * FROM productos ORDER BY id ASC')
     return render_template('index.html', productos=productos)
 
-# Ruta para dar "Like" sin recargar la página (AJAX)
+# Ruta para dar "Me gusta" sin recargar la página (AJAX)
 @app.route('/like/<int:producto_id>', methods=['POST'])
 def dar_like(producto_id):
     # Cambiado el "?" de SQLite por "%s" que usa PostgreSQL
     query_db('UPDATE productos SET likes = likes + 1 WHERE id = %s', [producto_id])
     res = query_db('SELECT likes FROM productos WHERE id = %s', [producto_id], one=True)
-    return jsonify({'success': True, 'likes': res['likes']})
+    return jsonify({'exito': True, 'likes': res['likes']})
 
 # Ruta para el botón de pedir: Genera el texto para WhatsApp
 @app.route('/pedir/<int:producto_id>')
 def pedir(producto_id):
-    p = query_db('SELECT * FROM productos WHERE id = %s', [producto_id], one=True)
-    if p:
-        mensaje = f"¡Hola, Sr. Campero! 🍗 Quisiera ordenar para llevar:\n\n• *{p['nombre']}* (${p['precio']:.2f})\n\nMe gustaría pasar a recogerlo en unos minutos. ¿Me confirman el tiempo de espera?"
-        mensaje_enconded = urllib.parse.quote(mensaje)
-        return redirect(f"https://wa.me/529616768289?text={mensaje_enconded}")
+    pag = query_db('SELECT * FROM productos WHERE id = %s', [producto_id], one=True)
+    if pag:
+        mensaje = f"¡Hola, Sr. Campero! 🍗 Quisiera ordenar para llevar:\n\n* *{pag['nombre']}* * (${pag['precio']:.2f})\n\nMe gustaría pasar a recogerlo."
+        mensaje_encriptado = urllib.parse.quote(mensaje)
+        return redirect(f"https://wa.me/529616768289?text={mensaje_encriptado}")
     return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
